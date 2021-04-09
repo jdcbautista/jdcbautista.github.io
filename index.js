cc49cf52b7c0095d2ac8c4488d5c0af9db8f11ed
Julius <68971573+jdcbautista@users.noreply.github.com>
2021-04-09 11:47:49 -0500

    Update index.js

--- index.js
+++ index.js
@@ -430,8 +430,8 @@ function followMouse(event) {
 addEventListener('mousemove', event => {
  let mousePos = getMousePos(canvas, event);
   
-    player.x = (player.x + mousePos.x) / 2;
-    player.y = (player.y + mousePos.y) / 2;
+    player.x = ((player.x + mousePos.x) / 2) - (mousePos.x - player.x);
+    player.y = ((player.y + mousePos.y) / 2) - (mousePos.y - player.y;
     console.log(mousePos.x,mousePos.y)
 });
                  
