38d7080d2cb54d914e55897f9ab753e5d108d0b5
Julius <68971573+jdcbautista@users.noreply.github.com>
2021-04-09 12:23:34 -0500

    Update index.js

--- index.js
+++ index.js
@@ -437,9 +437,9 @@ addEventListener('mousemove', event => {
  } else if (mousePos.x < player.x) {
     offsetX -= 20} 
   if (mousePos.y > player.y) {
-    offsetX += 20
+    offsetX -= 20
  } else if (mousePos.y < player.y) {
-    offsetX -= 20} 
+    offsetX += 20} 
 
     
     player.x = ((player.x + mousePos.x) / 2) - offsetX;
