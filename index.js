7cec94bb53abdbf918180e21323d1d2f183a1a95
Julius <68971573+jdcbautista@users.noreply.github.com>
2021-04-09 09:05:59 -0500

    Update index.js

--- index.js
+++ index.js
@@ -413,39 +413,6 @@ addEventListener('keydown', event => {
     console.log(`Key "d" repeating  [event: keydown]`); }
 });
 
-
-
-
-function() {
-    document.onmousemove = handleMouseMove;
-    function handleMouseMove(event) {
-        var eventDoc, doc, body;
-
-        event = event || window.event; // IE-ism
-
-        // If pageX/Y aren't available and clientX/Y are,
-        // calculate pageX/Y - logic taken from jQuery.
-        // (This is to support old IE)
-        if (event.pageX == null && event.clientX != null) {
-            eventDoc = (event.target && event.target.ownerDocument) || document;
-            doc = eventDoc.documentElement;
-            body = eventDoc.body;
-
-            event.pageX = event.clientX +
-              (doc && doc.scrollLeft || body && body.scrollLeft || 0) -
-              (doc && doc.clientLeft || body && body.clientLeft || 0);
-            event.pageY = event.clientY +
-              (doc && doc.scrollTop  || body && body.scrollTop  || 0) -
-              (doc && doc.clientTop  || body && body.clientTop  || 0 );
-        }
-
-        // Use event.pageX / event.pageY here
-        player.x = event.pageX;
-        player.y = event.pageY;
-    }
-};
-
-
 addEventListener('click', () => {
   const angle = Math.atan2(
     event.clientY - player.y,
